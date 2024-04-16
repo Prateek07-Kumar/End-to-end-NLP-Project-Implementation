5f886880cbea38b2d7c8b0b98c639f3eccef85dc
Prateek07-Kumar <prateekchaurasia791@gmail.com>
2024-04-16 21:30:42 +0530

    data_file_added

--- demo.py
+++ demo.py
@@ -1,3 +1,12 @@
 from hate.logger import logging
+from hate.exception import CustomException
 
-logging.info("Welcome to the project")
+
+# logging.info("Welcome to the project")
+
+
+try:
+    a = 7 / "0"
+    
+except Exception as e:
+    raise CustomException(e, sys) from e 
